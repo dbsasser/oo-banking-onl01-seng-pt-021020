29fb77c9e86f465d029750558637f97b6f453001
Daniel Sasser <danielbsasser@gmail.com>
2020-02-20 04:12:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if @status == "pending" && @sender.balance > @amount && @sender.valid?
+    if @status == "pending" && @receiver.balance > @amount && @sender.valid?
         @sender.balance -= @amount
         @receiver.balance += @amount
         @status = "complete"
